3c8fb7190808045d283a1eb1248618a071282cb1
shatabdi <mshatabdii03@gmail.com>
2026-02-04 17:00:57 +0530

    Loaded 3D model and added lights

--- macbook-gsap-app/src/App.jsx
+++ macbook-gsap-app/src/App.jsx
@@ -2,12 +2,14 @@
 
 import Hero from "./components/Hero"
 import NavBar from "./components/NavBar"
+import ProductViewer from "./components/ProductViewer"
 
 const App = () => {
   return (
     <main>
         <NavBar/>
         <Hero/>
+        <ProductViewer/>
     </main>
   )
 }

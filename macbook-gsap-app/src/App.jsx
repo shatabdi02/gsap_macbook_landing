8a0800f5926b1cf61dfc740654d077cf68fc847a
shatabdi <mshatabdii03@gmail.com>
2026-02-04 11:15:15 +0530

    Initial Commit

---
+++ macbook-gsap-app/src/App.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+import NavBar from "./components/NavBar"
+
+const App = () => {
+  return (
+    <main>
+        <NavBar/>
+    </main>
+  )
+}
+
+export default App

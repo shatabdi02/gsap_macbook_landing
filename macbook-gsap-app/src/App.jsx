48dc48bfd79800d40405e43ae043730aa0278ade
shatabdi <mshatabdii03@gmail.com>
2026-02-04 11:54:57 +0530

    Implement hero section

--- macbook-gsap-app/src/App.jsx
+++ macbook-gsap-app/src/App.jsx
@@ -1,11 +1,13 @@
 // import React from 'react'
 
+import Hero from "./components/Hero"
 import NavBar from "./components/NavBar"
 
 const App = () => {
   return (
     <main>
         <NavBar/>
+        <Hero/>
     </main>
   )
 }
